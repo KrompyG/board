cdd0293f5ade944b5fd69639fe465ad2f6df1922
Max Platonov <xamtalp@gmail.com>
2019-09-12 19:31:59 +0300

    Started working with SQLAlchemy

--- app_folder/__init__.py
+++ app_folder/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Dev_config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Dev_config)
+db = SQLAlchemy(app)
+migrate = Migrate(db, app)
 
-from app_folder import routes
+from app_folder import routes, models
