71c8bd9418f0ea95dba913bebbe1dd7a8d50420c
Max Platonov <xamtalp@gmail.com>
2019-09-13 00:31:09 +0300

    Created database (migrations)

--- app_folder/models.py
+++ app_folder/models.py
@@ -2,8 +2,8 @@ from app_folder import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(64), unique = True)
-    email = db.Column(db.String(120), unique = True)
+    username = db.Column(db.String(64), index=True, unique = True)
+    email = db.Column(db.String(120), index=True, unique = True)
     password_hash = db.Column(db.String(128))
 
     def __repr__ (self):
