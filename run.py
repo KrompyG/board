16b535264f7aa10815ebc63dab5e93d0554e07b3
Max Platonov <xamtalp@gmail.com>
2019-09-14 16:54:35 +0300

    Added shell_context

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-from app_folder import app
+from app_folder import app, db
+from app_folder.models import User, Product
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Product': Product}
 
 app.run()
