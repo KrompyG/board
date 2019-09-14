bebb0dda4a829a26b432714a1a9355160f263944
Max Platonov <xamtalp@gmail.com>
2019-09-14 15:52:36 +0300

    Product table

--- app_folder/models.py
+++ app_folder/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app_folder import db
 
 class User(db.Model):
@@ -5,6 +6,16 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique = True)
     email = db.Column(db.String(120), index=True, unique = True)
     password_hash = db.Column(db.String(128))
+    products = db.relationship('Post', backref = 'owner', lazy = 'dynamic')
 
     def __repr__ (self):
         return '<User {}>'.format(self.username)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64))
+    timestamp = db.Column(db.DateTime, index = True, default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Product {}>'.format(self.name)
