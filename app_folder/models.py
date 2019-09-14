97ba858debfe8159df290625b0cffa2f8a63e6cb
Max Platonov <xamtalp@gmail.com>
2019-09-14 16:00:03 +0300

    Typo_fix

--- app_folder/models.py
+++ app_folder/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique = True)
     email = db.Column(db.String(120), index=True, unique = True)
     password_hash = db.Column(db.String(128))
-    products = db.relationship('Post', backref = 'owner', lazy = 'dynamic')
+    products = db.relationship('Product', backref = 'owner', lazy = 'dynamic')
 
     def __repr__ (self):
         return '<User {}>'.format(self.username)
