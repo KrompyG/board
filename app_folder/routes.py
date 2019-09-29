625cdaa30c405862d6ce98508d048651ca002a6c
Max Platonov <xamtalp@gmail.com>
2019-09-30 01:56:31 +0300

    added product photo display

--- app_folder/routes.py
+++ app_folder/routes.py
@@ -1,11 +1,18 @@
-from flask import render_template, flash, redirect, url_for
+import os
+import uuid
+from flask import render_template, flash, redirect, url_for, request
 from app_folder import app
 from app_folder.forms import Login_form, Add_product_form, Register_form
 from flask_login import current_user, login_user, logout_user, login_required
 from app_folder.models import User, Product
-from flask import request
 from werkzeug.urls import url_parse
+from werkzeug.utils import secure_filename
 from app_folder import db
+
+# checking file's extension
+def allowed_file(filename):
+    return ('.' in filename and
+            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS'])
 
 @app.route('/')
 @app.route('/index')
@@ -41,6 +48,15 @@ def add_product():
         product = Product(name = form.productname.data,
                           category_id = form.category.data,
                           user_id = current_user.id)
+
+        photo = form.photo.data
+        if photo and allowed_file(photo.filename):
+            extension = photo.filename.rsplit('.', 1)[1]
+            photo_path = os.path.join('static', 'img', 'products',
+                                      str(uuid.uuid4().hex) + '.' + extension)
+            photo.save(os.path.join(app.config['BASEDIR'], 'app_folder', photo_path))
+            product.path_to_photo = photo_path
+
         db.session.add(product)
         db.session.commit()
         flash('Новый продукт {} успешно добавлен!'.format(
@@ -73,6 +89,6 @@ def logout():
 
 @app.route('/my_offers', methods=['GET', 'POST'])
 @login_required
-def get_my_products():
+def my_offers():
     offers = Product.query.filter_by(owner = current_user).order_by(Product.category_id.asc())
     return render_template('my_offers.html', offers = offers)
--- config.py
+++ config.py
@@ -6,3 +6,5 @@ class Dev_config(object):
     SQLALCHEMY_DATABASE_URI = (os.environ.get('DATABASE_URL') or
                                'sqlite:///' + os.path.join(basedir, 'app.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    BASEDIR = basedir
+    ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
